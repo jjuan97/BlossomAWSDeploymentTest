b78cafd0c6221136ed21ecb02ff7a14d59d66634
jonatan <jbuitrago@blossom.net>
2024-05-24 14:28:36 -0500

    feat: initil

---
+++ test-api/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Wait for the PostgreSQL container to be ready
+until nc -z -v -w30 testPostgresSQLContainer 5432
+do
+  echo "Waiting for PostgreSQL database connection..."
+  sleep 1
+done
+
+# Run database creation and migration
+npx sequelize-cli db:create
+npx sequelize-cli db:migrate
+
+# Start the application
+npm run start
