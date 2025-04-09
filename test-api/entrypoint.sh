2025b8aedcc654ebd4ab888be8d37786b231664d
jjuan97 <juanjosechepe@gmail.com>
2025-04-08 20:07:55 -0500

    [FEATURE] Modify entry point

--- test-api/entrypoint.sh
+++ test-api/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# Wait for the PostgreSQL container to be ready
-until nc -z -v -w30 testPostgresSQLContainer 5432
-do
-  echo "Waiting for PostgreSQL database connection..."
-  sleep 1
-done
-
 # Run database creation and migration
 npx sequelize-cli db:migrate
 npm install
